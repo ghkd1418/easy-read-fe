116fcee968f2ef830bb6a693da88a644193bdf41
ghkd1418 <1004wipi@gmail.com>
2024-07-22 19:52:17 +0900

    tds

--- src/features/simplification/lib/useTextArea.ts
+++ src/features/simplification/lib/useTextArea.ts
@@ -11,7 +11,9 @@ export const useTextArea = () => {
     }
 
     const getGptAnswer = async (text: string) => {
-        const eventSource = new EventSource(`https://geulmatchum-api.store/gpt/stream?text=${encodeURIComponent(text)}`)
+        const eventSource = new EventSource(
+            `${import.meta.env.VITE_API_HOST}/gpt/stream?text=${encodeURIComponent(text)}`,
+        )
 
         eventSource.onmessage = (event) => {
             try {
